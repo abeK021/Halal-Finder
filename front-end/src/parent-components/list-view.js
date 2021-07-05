304aeba59b7389b3266be589f7ae6d154b2d6da4
abeakaid <74392388+abeK021@users.noreply.github.com>
2021-07-05 13:12:33 -0400

    list item styling pressed

--- front-end/src/parent-components/list-view.js
+++ front-end/src/parent-components/list-view.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react"
+import React, {useState, useRef, useEffect} from "react"
 import { useSelector } from "react-redux"
 import 'bootstrap'
 import './list-view-style.css'
@@ -7,30 +7,45 @@ import Filteration from '../child-components/filter-search/pagination'
 
 import isEmpty from 'lodash.isempty'
 
+
+
+
+
 const ListViewTab =() => {
 
+  const mainRef = useRef(null)
+
+  const initialfocus = (e) => {
+    e.target.className = 'container-fluid row active-item'
+  };
+
+  const noFocus = (e) => {
+    e.target.className = 'container-fluid row'
+  };
+
   let listItem;
+
 
   const data = useSelector(state =>  state.restaurants.restaurants)
 
   const [listInfo, setListInfo] = useState(null)
   
-  const listClick = () => {
-    
+  const listClick = (e) => {
+   
   }
  
   const renderRestaurantList = () => {
         return  (
       <div>
-          <ul className="restaurant-list-container" >
+          <ul className="restaurant-list-container " >
             {
             !isEmpty(data) ?
             data.map((restaurant) => 
               
-            <div>
-              <div className="container-fluid row" onClick={() => setListInfo({name: restaurant.name, geo: restaurant.location.geo, address: `${restaurant.location.street} ${restaurant.location.cityState}, ${restaurant.location.zip}`, phone: String(restaurant.number), description: restaurant.description, website: restaurant.website,})}>
-              <div className="img-container col"><img key={restaurant.name} src={restaurant.imgUrl} alt=""></img></div>
-              <p key={restaurant.name} className="restaurant-list col ">{restaurant.name}</p>
+            <div className="clicked-list-item">
+              <div tabIndex={-1} className="container-fluid row "  onClick={() => setListInfo({name: restaurant.name, geo: restaurant.location.geo, address: `${restaurant.location.street} ${restaurant.location.cityState}, ${restaurant.location.zip}`, phone: String(restaurant.number), description: restaurant.description, website: restaurant.website,})}>
+                  <div className="img-container col"><img key={restaurant.name} src={restaurant.imgUrl} alt=""></img></div>
+                  <p key={restaurant.name} className="restaurant-list col mt-4">{restaurant.name}</p>
               </div>
               <hr/>
             </div>
