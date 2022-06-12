d26c6018d14c7561f076c4cfdd0c796a0de5764a
abeakaid <74392388+abeK021@users.noreply.github.com>
2022-06-11 21:57:51 -0400

    visable api keys for testing

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,10 @@
 require("dotenv").config();
 module.exports = {
-  MONGODB_URI: process.env.MONGODB_URI,
-  GOOGLE_API_KEY: process.env.GOOGLE_API_KEY,
-  GOOGLE_PLACES_API_KEY: process.env.GOOGLE_PLACES_API_KEY,
+  // MONGODB_URI: process.env.MONGODB_URI,
+  // GOOGLE_API_KEY: process.env.GOOGLE_API_KEY,
+  // GOOGLE_PLACES_API_KEY: process.env.GOOGLE_PLACES_API_KEY,
+
+  MONGODB_URI: "mongodb://127.0.0.1/resteraunts",
+  GOOGLE_API_KEY: "AIzaSyDDZ5Inw8robmEARXPZ5zegTlCl2lCBJVg",
+  GOOGLE_PLACES_API_KEY: "AIzaSyDHodYPfHlFQmhCxoQkFXzPSVLR4XBbdRE",
 };
