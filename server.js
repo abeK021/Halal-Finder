aae4c7799b4ce48fea464b2b36689c89a44c5020
abeakaid <74392388+abeK021@users.noreply.github.com>
2022-06-13 02:16:35 -0400

    accessing path before initilization

--- server.js
+++ server.js
@@ -28,11 +28,12 @@ app.use(routes);
 if (process.env.NODE_ENV === "production") {
   // Express will serve up production assets
   // like our main.js file, or main.css file!
+  const path = require("path");
   app.use(express.static(path.join(__dirname, "client/build")));
 
   // Express will serve up the index.html file
   // if it doesn't recognize the route
-  const path = require("path");
+
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
