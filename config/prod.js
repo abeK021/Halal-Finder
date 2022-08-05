3a8e7eddc1b8eeaa2112cd30a25dd107f89dba43
abeakaid <74392388+abeK021@users.noreply.github.com>
2022-08-05 16:49:36 -0400

    ignore dev cause of env keys

--- config/prod.js
+++ config/prod.js
@@ -3,9 +3,4 @@ module.exports = {
   GOOGLE_API_KEY: process.env.GOOGLE_API_KEY,
   GOOGLE_PLACES_API_KEY: process.env.GOOGLE_PLACES_API_KEY,
   EXTREME_IP_LOOKUP_KEY: process.env.EXTREME_IP_LOOKUP_KEY,
-
-  // MONGODB_URI: "mongodb://127.0.0.1/resteraunts",
-  // GOOGLE_API_KEY: "AIzaSyDDZ5Inw8robmEARXPZ5zegTlCl2lCBJVg",
-  // GOOGLE_PLACES_API_KEY: "AIzaSyDHodYPfHlFQmhCxoQkFXzPSVLR4XBbdRE",
-  // EXTREME_IP_LOOKUP_KEY: "aDN4ReKjA1YM8bWQxbkD",
 };
